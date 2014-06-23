cac252b4adc59a62a80abd94acf0e8b367d7d13a
Amy Hanlon <amyehanlon@gmail.com>
2014-06-22 21:35:52 -0400

    switch to argparse

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
+import argparse
 import socket
+import sys
 
 
 if __name__ == '__main__':
-    host = 'localhost'
-    port = 5000
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', nargs='?', default='localhost')
+    parser.add_argument('port', nargs='?', type=int, default=5000)
+    args = parser.parse_args()
     size = 1024
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,port))
+    s.connect((args.host, args.port))
     s.send('hello world')
     data = s.recv(size)
     s.close()
